2b2b965674d3cbb79b95f78924db70ef8af25679
Josh <josharmstrongphoto@gmail.com>
2018-01-25 09:28:58 -0800

    Initial commit

---
+++ CS-006/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_006.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Simple Calculator</h1>
+        <p>
+            First Value:&nbsp;
+            <asp:TextBox ID="firstValueTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Second Value:&nbsp;
+            <asp:TextBox ID="secondValueTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="+" />
+&nbsp;
+            <asp:Button ID="subButton" runat="server" OnClick="subButton_Click" Text="-" />
+&nbsp;
+            <asp:Button ID="multButton" runat="server" OnClick="multButton_Click" Text="*" />
+&nbsp;
+            <asp:Button ID="divButton" runat="server" OnClick="divButton_Click" Text="/" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ CS-006/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_006
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(firstValueTextBox.Text);
+            int b = Convert.ToInt32(secondValueTextBox.Text);
+            int c = a + b;
+
+            resultLabel.Text = c.ToString();
+        }
+
+        protected void subButton_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(firstValueTextBox.Text);
+            int b = Convert.ToInt32(secondValueTextBox.Text);
+            int c = a - b;
+
+            resultLabel.Text = c.ToString();
+        }
+
+        protected void multButton_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(firstValueTextBox.Text);
+            int b = Convert.ToInt32(secondValueTextBox.Text);
+            int c = a * b;
+
+            resultLabel.Text = c.ToString();
+        }
+
+        protected void divButton_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(firstValueTextBox.Text);
+            int b = Convert.ToInt32(secondValueTextBox.Text);
+            int c = a / b;
+
+            resultLabel.Text = c.ToString();
+        }
+    }
+}
